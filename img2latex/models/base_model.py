ca58969edae24b0b8a269b971f4307eaf576ac30
zeinovich <nikita.zinovich@gmail.com>
2023-10-19 17:24:57 +0300

    ABC for Im2Seq models

---
+++ img2latex/models/base_model.py
@@ -0,0 +1,19 @@
+# Abstract Base Class for model
+
+import torch
+import torch.nn as nn
+
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class BaseIm2SeqModel(ABC, nn.Module):
+    @abstractmethod
+    def predict(self, x: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractproperty
+    def device(self) -> torch.device:
+        pass
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.forward(x)
