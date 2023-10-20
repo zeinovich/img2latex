728a9e776858938ac2f9473ed15a3d3bb85672a9
zeinovich <nikita.zinovich@gmail.com>
2023-10-20 12:00:59 +0300

    changes cnn_lstm architecture

--- img2latex/models/cnn_lstm/cnn_lstm.py
+++ img2latex/models/cnn_lstm/cnn_lstm.py
@@ -1,28 +1,59 @@
 import torch
 import torch.nn as nn
-from torchvision.models import (
-    ResNet18_Weights,
-    ResNet34_Weights,
-    ResNet50_Weights,
-    ResNet101_Weights,
-)
 
 from ..base_model import BaseIm2SeqModel
-import sys
 
-# [TODO] add device member
-# [TODO] add image_size member
 # [TODO] add model ABC
 
 
-class ResnetLSTM(BaseIm2SeqModel):
+class ConvBlock(nn.Module):
+    """
+    Simple 3x3 conv with padding size 1 (to leave the input size unchanged), followed by a ReLU.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: int = 1,
+    ) -> None:
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Parameters
+        ----------
+        x
+            of dimensions (B, C, H, W)
+
+        Returns
+        -------
+        torch.Tensor
+            of dimensions (B, C, H, W)
+        """
+        c = self.conv(x)
+        r = self.relu(c)
+        return r
+
+
+class CNNLSTM(BaseIm2SeqModel):
     def __init__(
         self,
         resnet_depth: int = 18,
-        emb_dim: int = 16,
-        hidden_dim: int = 128,
+        emb_dim: int = 24,
+        hidden_dim: int = 512,
         dropout_prob: float = 0,
-        vocab_len: dict[str, int] = None,
+        vocab_len: int = None,
         max_output_length: int = 512,
         device: str = "cpu",
     ) -> None:
@@ -35,10 +66,12 @@ class ResnetLSTM(BaseIm2SeqModel):
             101,
         ], "resnet_depth must be one of [18, 34, 50, 101]"
 
+        # At least special tokens must be present
+        assert vocab_len > 4, "Vocabulary length must be at least 4"
+
         self._encoder_out = hidden_dim
         self._hidden_dim = hidden_dim
         self._vocab_len = vocab_len
-
         # standard vocabulary with special tokens
         self._vocab = {
             "<UNK>": 0,
@@ -51,24 +84,27 @@ class ResnetLSTM(BaseIm2SeqModel):
         self._emb_dim = emb_dim
         self._max_len = max_output_length
         self._device = device
+        self._img_size = 256
 
         # import weights from corresponding module
-        weights = getattr(
-            sys.modules[__name__], f"ResNet{resnet_depth}_Weights"
-        ).DEFAULT
-
-        # load model and weights from torch.hub
-        self.encoder = torch.hub.load(
-            "pytorch/vision:v0.10.0",
-            f"resnet{resnet_depth}",
-            weights=weights,
-        )
-
-        for param in self.encoder.parameters():
-            param.requires_grad = False
-
-        self.encoder.fc = nn.Sequential(
-            nn.Linear(self.encoder.fc.in_features, self._encoder_out)
+        self.encoder = nn.Sequential(  # img_size = 256
+            ConvBlock(in_channels=1, out_channels=32),
+            ConvBlock(in_channels=32, out_channels=32),
+            nn.AvgPool2d(2, 2),  # img_size = 128
+            ConvBlock(in_channels=32, out_channels=64),
+            ConvBlock(in_channels=64, out_channels=64),
+            nn.AvgPool2d(2, 2),  # img_size = 64
+            ConvBlock(in_channels=64, out_channels=128),
+            ConvBlock(in_channels=128, out_channels=128),
+            nn.AvgPool2d(2, 2),  # img_size = 32
+            ConvBlock(in_channels=128, out_channels=256),
+            ConvBlock(in_channels=256, out_channels=256),
+            nn.AvgPool2d(2, 2),  # img_size = 16
+            ConvBlock(in_channels=256, out_channels=256),
+            nn.MaxPool2d(2, 2),  # img_size = 8
+            # (B, 256, 8, 8)
+            nn.Flatten(),
+            nn.Linear(in_features=256 * 8 * 8, out_features=self._encoder_out),
         )
 
         self.decoder = nn.LSTM(
@@ -77,21 +113,21 @@ class ResnetLSTM(BaseIm2SeqModel):
             num_layers=1,
             batch_first=True,
         )
-        self.init_hidden = nn.Linear(self._encoder_out, self._hidden_dim)
-        self.init_cell = nn.Linear(self._encoder_out, self._hidden_dim)
-        self.init_output = nn.Linear(self._encoder_out, self._hidden_dim)
 
         self.dropout = nn.Dropout(p=dropout_prob)
         self.embedding = nn.Embedding(
             self._output_dim, self._emb_dim, padding_idx=self._vocab["<PAD>"]
         )
-        self.fc_out = nn.Linear(self._hidden_dim, self._output_dim)
+        self.fc_out = nn.Sequential(
+            nn.Linear(self._hidden_dim, self._hidden_dim),
+            nn.Linear(self._hidden_dim, self._output_dim),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size = x.size(0)
         encoded_img = self.encoder(x)
 
-        # outputs of shape (B, MAX_LEN, VOCAB_len_SIZE (OUTPUT_DIM))
+        # outputs of shape (B, MAX_LEN, VOCAB_SIZE (OUTPUT_DIM))
         outputs = (
             torch.ones(
                 batch_size, self._max_len, self._output_dim, requires_grad=True
@@ -106,9 +142,9 @@ class ResnetLSTM(BaseIm2SeqModel):
             * self._vocab["<SOS>"]
         )
 
-        hidden = self.init_hidden(encoded_img)
-        cell = self.init_cell(encoded_img)
-        output = self.init_output(encoded_img)
+        hidden = encoded_img
+        cell = torch.zeros(batch_size, self._hidden_dim)
+        output = torch.zeros(batch_size, self._hidden_dim)
 
         for t in range(1, self._max_len):
             hidden, cell, output, logit = self.decode(
@@ -120,9 +156,6 @@ class ResnetLSTM(BaseIm2SeqModel):
 
             outputs[:, t, ...] = logit
             input_token = torch.argmax(logit, 1)
-
-            if input_token.item() == self._vocab["<EOS>"]:
-                break
 
         return outputs
 
@@ -144,6 +177,9 @@ class ResnetLSTM(BaseIm2SeqModel):
         batch_size = x.size(0)
         encoded_img = self.encoder(x)
 
+        hidden = encoded_img
+        cell = torch.zeros(batch_size, self._hidden_dim)
+        output = torch.zeros(batch_size, self._hidden_dim)
         # outputs of shape (B, MAX_LEN, VOCAB_LEN (OUTPUT_DIM))
         outputs = (
             torch.ones(batch_size, self._max_len, self._output_dim)
@@ -157,9 +193,6 @@ class ResnetLSTM(BaseIm2SeqModel):
             torch.ones(batch_size, 1).type_as(x).to(self._device).long()
             * self._vocab["<SOS>"]
         )
-        output = encoded_img
-        hidden = self.init_hidden(encoded_img)
-        cell = self.init_cell(encoded_img)
 
         for t in range(1, self._max_len):
             hidden, cell, output, logit = self.decode(
@@ -191,3 +224,7 @@ class ResnetLSTM(BaseIm2SeqModel):
     @property
     def device(self):
         return self._device
+
+    @property
+    def img_size(self):
+        return self._img_size
